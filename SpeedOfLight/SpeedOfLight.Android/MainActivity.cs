e24ec9e56c30548b11dc0f38ed847ca634797a37
Saamer Mansoor <i@saamer.me>
2020-02-06 01:27:17 -0500

    Changed Solution Configuration Mapping, Fixed MainActivity

--- SpeedOfLight/SpeedOfLight.Android/MainActivity.cs
+++ SpeedOfLight/SpeedOfLight.Android/MainActivity.cs
@@ -6,22 +6,28 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Comet.Android;
 
 namespace SpeedOfLight.Droid
 {
     [Activity(Label = "SpeedOfLight", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : CometActivity// global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+#if DEBUG
+            Comet.Reload.Init();
+#endif
+            //Comet.Skia.UI.Init();
+
+            //Replaces native controls with Skia drawn controls
+            //Comet.Skia.Controls.Init ();
+
+            Page = new MainPage();
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
